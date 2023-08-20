5e611397dfd5343fe9edea2517646664c2da9fac
Prakash <137217635+prakash4mthevriksha@users.noreply.github.com>
2023-08-20 06:55:53 +0530

    manually uploaded assignment files
    manually uploaded assignment files

---
+++ jQueryScript.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  console.log("Script executed!"); // Add this line
+  $(".get-a-stunning-website").click(function () {
+    var url = "https://www.thevriksha.in/food-truck-website-development-services/";
+    window.open(url, '_self');
+  });
+});
+
+$(document).ready(function () {
+  $(".optimize-for-success").click(function () {
+    var url = "https://www.thevriksha.in/digital-marketing-for-food-truck-business/";
+    window.open(url, '_self');
+  });
+});
+$(document).ready(function () {
+  $(".ignite-social-growth").click(function () {
+    var url = "https://www.thevriksha.in/social-media-marketing-for-food-truck-business/";
+    window.open(url, '_self');
+  });
+});
+$(document).ready(function () {
+  $(".committed-to-delivering-the-success-to-food-truck-business-owners").click(function () {
+    var url = "https://www.thevriksha.in/food-truck-website-development-services/";
+    window.open(url, '_self');
+  });
+});
+$(document).ready(function () {
+  $(".committed-to-implementing-the-best-practices-based-SEO-strategy-for-the-food-truck-business-owners").click(function () {
+    var url = "https://www.thevriksha.in/digital-marketing-for-food-truck-business/";
+    window.open(url, '_self');
+  });
+});
+$(document).ready(function () {
+  $(".gain-social-media-followers").click(function () {
+    var url = "https://www.thevriksha.in/social-media-marketing-for-food-truck-business/";
+    window.open(url, '_self');
+  });
+});
